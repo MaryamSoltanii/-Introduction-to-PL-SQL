e6932a37a9f7b98f4d3130e3e63c4b0b63427600
Maryam Soltani <141626334+MaryamSoltanii@users.noreply.github.com>
2025-09-10 18:26:43 +0330

    Update tax_calc.sql

--- code/tax_calc.sql
+++ code/tax_calc.sql
@@ -1,9 +1,1 @@
-DECLARE
-  v_tax NUMBER;
-BEGIN
-  v_tax := p_salary * 0.1;
-  DBMS_OUTPUT.PUT_LINE('Salary: ' || p_salary || ', Tax: ' || v_tax);
-EXCEPTION
-  WHEN OTHERS THEN
-    DBMS_OUTPUT.PUT_LINE('Error in calculation');
-END;
+
